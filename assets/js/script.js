9fa427428c647753e8113db601b9d961041c3af3
Sean Kelly <invertedrdm@gmail.com>
2021-03-15 19:31:03 -0400

    background color of input box reflects whether it is before, during, or after time page was loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
+var ctTime = moment('13','H').format('H');
+
 function update() {
     var cTime = moment();
     $('#currentDay').text(cTime.format('dddd, Do MMMM H:mm:ss'));
-  }
+}
   setInterval(update, 1000);
+
+for(var i = 9; i<18; i++){
+    console.log(ctTime);
+    console.log(('#' + i));
+    if(i > ctTime){
+        console.log('green')
+        $("#"+i).addClass("bg-success");
+    }
+    else if(i < ctTime){
+        console.log('gray');
+        $("#"+i).addClass("bg-secondary");
+    }
+    else{
+        console.log('red')
+        $("#"+i).addClass("bg-danger");
+    }
+}
