305cd88fc6faf1dee71d33fd7f285465fa02380f
Sean Kelly <invertedrdm@gmail.com>
2021-03-15 22:14:15 -0400

    added js comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
+// sets a universal variable for time as of page load
 var ctTime = moment().format('H');
 
+// on page load grabs saved text from local storage and prints it to input boxes
 function startUp(){
     for(var j = 9; j<18; j++){
     var tempGrab = ('#' + j);
     var tempString = localStorage.getItem(tempGrab);
-    console.log(tempGrab);
-    console.log(tempString);
     $(tempGrab).val(tempString);
     }
 }
+
+// checks time and updates top of page every second
 function update() {
     var cTime = moment();
     $('#currentDay').text(cTime.format('dddd, Do MMMM'));
 }
   setInterval(update, 1000);
 
+  // checks to see if the current time is before, during, or after slotted time for the card
+  // then changes the background color to indicate that state
 for(var i = 9; i<18; i++){
     if(i > ctTime){
         $('#'+i).addClass('bg-success');
+
     }
     else if(i < ctTime){
         $('#'+i).addClass('bg-secondary');
@@ -27,6 +32,7 @@ for(var i = 9; i<18; i++){
     }
 }
 
+// saves text in all input boxes to local storage on click
 $("a").click(function(event) {
     event.preventDefault();
     for(var i = 9; i<18; i++){
@@ -35,4 +41,5 @@ $("a").click(function(event) {
     }
 });
 
+// makes sure saved text populates on page load
 startUp();
